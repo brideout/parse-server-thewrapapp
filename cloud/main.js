f2bc9643da8fea2172e2548da15c0c044a3ab99f
brideout <bret@rapidwarellc.com>
2017-03-28 22:46:51 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -5,22 +5,6 @@ Parse.Cloud.define('hello', function(req, res) {
 
 Parse.Cloud.define("updateExpDate", function(request, response){
   var query = new Parse.Query(Parse.User);
-  query.equalTo('objectId', request.params.objectId);
-  query.find({
-    useMasterKey: true,
-    success: function(results){
-      if(results.length>0){
-        var user = results[0];
-        user.set("SOMEPARAMETER",true);
-        user.save().then(
-            function(result){
-            },
-            function(error){
-                console.log("Error: " + error.code + " " + error.message);
-            });
-        }
-      }
-    },
     error: function(error){
             response.error('query error: '+ error.code + " : " + error.message);
     }
