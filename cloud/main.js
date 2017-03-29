fecefb0f4800272d7cc9c162406e565805b65517
brideout <bret@rapidwarellc.com>
2017-03-28 22:43:34 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,27 +2,3 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
-
-Parse.Cloud.define("updateExpDate", function(request, response){
-  var query = new Parse.Query(Parse.User);
-  query.equalTo('customerId', request.params.customerId);
-  query.find({
-    useMasterKey: true,
-    success: function(results){
-      if(results.length>0){
-        var user = results[0];
-        user.set("SOMEPARAMETER",true);
-        user.save().then(
-            function(result){
-            },
-            function(error){
-                console.log("Error: " + error.code + " " + error.message);
-            });
-        }
-      }
-    },
-    error: function(error){
-            response.error('query error: '+ error.code + " : " + error.message);
-    }
-  });
-});
