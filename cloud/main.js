37422030f2f498f61e25ee75db597c28e4603181
brideout <bret@rapidwarellc.com>
2017-03-28 23:36:31 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,23 +3,6 @@ Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
 
-Parse.Cloud.define("averageStars", function(request, response) {
-  var query = new Parse.Query("Review");
-  query.equalTo("movie", request.params.movie);
-  query.find({
-    success: function(results) {
-      var sum = 0;
-      for (var i = 0; i < results.length; ++i) {
-        sum += results[i].get("stars");
-      }
-      response.success(sum / results.length);
-    },
-    error: function() {
-      response.error("movie lookup failed");
-    }
-  });
-});
-
 Parse.Cloud.define("updateExpDate", function(request, response){
   var query = new Parse.Query(Parse.User);
   query.equalTo('customer_id', request.params.customerId);
@@ -28,7 +11,7 @@ Parse.Cloud.define("updateExpDate", function(request, response){
     success: function(results){
       if(results.length>0){
         var user = results[0];
-        user.set("ExpirationDate2",request.params.ExpirationDate);
+        user.set("ExpirationDate",request.params.ExpirationDate);
         user.save().then(
             function(result){
             },
