294d1a8652c76e9667f1c7e40648f8b95f80ca67
brideout <bret@rapidwarellc.com>
2017-03-28 23:15:27 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -22,7 +22,7 @@ Parse.Cloud.define("averageStars", function(request, response) {
 
 Parse.Cloud.define("modifyuser", function(request, response){
   var query = new Parse.Query(Parse.User);
-  query.equalTo('objectId', request.params.objectId);
+  query.equalTo('customer_id', request.params.customerId);
   query.find({
     useMasterKey: true,
     success: function(results){
