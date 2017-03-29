534c9fd6bc6a8e7bead46f33d8c7f9f9ad59b3f1
brideout <bret@rapidwarellc.com>
2017-03-28 22:48:08 -0500

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,11 +2,3 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
-
-Parse.Cloud.define("updateExpDate", function(request, response){
-  var query = new Parse.Query(Parse.User);
-    error: function(error){
-            response.error('query error: '+ error.code + " : " + error.message);
-    }
-  });
-});
